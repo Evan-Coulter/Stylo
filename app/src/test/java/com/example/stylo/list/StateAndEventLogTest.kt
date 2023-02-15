02f7f1c8be49796e7a2becea2e4f3b14c039a5a0
Evan-Coulter <evanrileycoulter@gmail.com>
2023-02-15 12:15:22 -0800

    add simple logging test

---
+++ app/src/test/java/com/example/stylo/list/StateAndEventLogTest.kt
@@ -0,0 +1,28 @@
+package com.example.stylo.list
+
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+
+@RunWith(RobolectricTestRunner::class)
+class StateAndEventLogTest {
+    @Test
+    fun simpleQueueTest() {
+        log(NoteListViewState.LoadingState)
+        log(NoteListViewState.ShowLogoEffect)
+        log(NoteListViewState.ShowSearchBar)
+        log(NoteListViewState.ShowHelpDialog)
+        log(NoteListViewState.ShowEmptySearchResult)
+
+        assertEquals(5, StateAndEventLog.states.size)
+        assertTrue(StateAndEventLog.states.values.last() is NoteListViewState.ShowEmptySearchResult)
+        assertTrue(StateAndEventLog.states.values.first() is NoteListViewState.LoadingState)
+
+        log(NoteListViewState.ShowSearchBar)
+
+        assertTrue(StateAndEventLog.states.values.last() is NoteListViewState.ShowSearchBar)
+        assertTrue(StateAndEventLog.states.values.first() is NoteListViewState.ShowLogoEffect)
+    }
+}
