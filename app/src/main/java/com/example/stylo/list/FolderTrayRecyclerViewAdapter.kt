58ba4aa8d9ee93bb6db4d605063f799db837fa08
Evan-Coulter <evanrileycoulter@gmail.com>
2023-03-13 19:19:45 -0700

    add display folder tray code

---
+++ app/src/main/java/com/example/stylo/list/FolderTrayRecyclerViewAdapter.kt
@@ -0,0 +1,40 @@
+package com.example.stylo.list
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.recyclerview.widget.RecyclerView
+import com.example.stylo.R
+import com.example.stylo.data.model.RoomFolder
+
+class FolderTrayRecyclerViewAdapter(
+    private val folders: Array<RoomFolder>,
+    private val onClickFolder: (Int)->Unit,
+    private val onLongPressFolder: (Int)->Unit
+) : RecyclerView.Adapter<FolderTrayRecyclerViewAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.folder_tray_list_item, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.folderButton.setOnClickListener {
+            onClickFolder(position)
+        }
+        holder.folderButton.setOnLongClickListener {
+            onLongPressFolder(position)
+            true
+        }
+        holder.folderButton.text = folders[position].name
+        //holder.folderButton.setBackgroundColor()
+    }
+
+    override fun getItemCount(): Int = folders.size
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val folderButton = view.findViewById<Button>(R.id.folder_tray_button)
+    }
+}
--- app/src/main/java/com/example/stylo/list/NoteListFragment.kt
+++ app/src/main/java/com/example/stylo/list/NoteListFragment.kt
@@ -4,17 +4,23 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
 import android.widget.ImageButton
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.stylo.MainApplication
 import com.example.stylo.R
+import com.example.stylo.data.model.RoomFolder
 import com.example.stylo.data.model.RoomNote
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+
 
 class NoteListFragment : Fragment() {
     private val viewModel: NoteListViewModel by viewModels {
@@ -46,7 +52,7 @@ class NoteListFragment : Fragment() {
     private fun onNewState(newState : NoteListViewState) {
         when (newState) {
             is NoteListViewState.ShowBasicListState -> showBasicListState(newState.notes)
-            is NoteListViewState.ShowFoldersTray -> showFoldersTray(newState.notes)
+            is NoteListViewState.ShowFoldersTray -> showFoldersTray(newState.folders)
             is NoteListViewState.ShowEditNoteDetailsOptions -> TODO()
             is NoteListViewState.ShowEditFolderDialog -> TODO()
             is NoteListViewState.LoadingState -> Toast.makeText(context, "Loading TODO", Toast.LENGTH_SHORT).show()
@@ -83,15 +89,53 @@ class NoteListFragment : Fragment() {
     private fun initButtons(view: View) {
         view.findViewById<ImageButton>(R.id.logo).setOnClickListener { Toast.makeText(context, "Logo clicked", Toast.LENGTH_SHORT).show() }
         view.findViewById<ImageButton>(R.id.helpButton).setOnClickListener { Toast.makeText(context, "Help clicked", Toast.LENGTH_SHORT).show() }
-        view.findViewById<ImageButton>(R.id.folder).setOnClickListener { Toast.makeText(context, "Folder clicked", Toast.LENGTH_SHORT).show() }
+        view.findViewById<ImageButton>(R.id.folder).setOnClickListener { viewModel._eventListener.value = NoteListEvent.FolderTrayButtonClicked }
         view.findViewById<ImageButton>(R.id.search).setOnClickListener { Toast.makeText(context, "Search clicked", Toast.LENGTH_SHORT).show() }
         view.findViewById<ImageButton>(R.id.list_card_switch).setOnClickListener { Toast.makeText(context, "Item type clicked", Toast.LENGTH_SHORT).show() }
         view.findViewById<FloatingActionButton>(R.id.fab).setOnClickListener {
             Toast.makeText(context, "Switch to editor", Toast.LENGTH_SHORT).show()
         }
+        view.findViewById<View>(R.id.note_list_fragment).setOnClickListener {
+            val folderTray: View = view.findViewById(R.id.folder_tray)
+            if (folderTray.isVisible) {
+                fadeOutView(folderTray)
+                folderTray.visibility = View.GONE
+            }
+        }
     }
 
-    private fun showFoldersTray() {
-        TODO()
+    private fun showFoldersTray(list: List<RoomFolder>) {
+        val folderTray = view?.findViewById<View>(R.id.folder_tray)
+        folderTray?.let{
+            it.setOnClickListener { /*do nothing*/ }
+            view?.findViewById<ImageView>(R.id.close_folder_tray_button)?.setOnClickListener { _ ->
+                fadeOutView(it)
+                it.visibility = View.GONE
+            }
+            fadeInView(it)
+            val recyclerView: RecyclerView = requireView().findViewById(R.id.folder_list)
+            recyclerView.layoutManager = LinearLayoutManager(context)
+            val adapter = FolderTrayRecyclerViewAdapter(
+                list.toTypedArray(),
+                onClickFolder = { id -> Toast.makeText(context, "$id folder clicked", Toast.LENGTH_SHORT).show() },
+                onLongPressFolder = { id -> Toast.makeText(context, "$id folder long pressed", Toast.LENGTH_SHORT).show() }
+            )
+            recyclerView.adapter = adapter
+        }
+    }
+
+    private fun fadeInView(view: View) {
+        view.visibility = View.INVISIBLE
+        val animation = AnimationUtils.loadAnimation(context, R.anim.fade_in)
+        view.startAnimation(animation)
+        view.visibility = View.VISIBLE
+    }
+
+    private fun fadeOutView(view: View) {
+        view.visibility = View.VISIBLE
+        val animation = AnimationUtils.loadAnimation(context, R.anim.fade_out)
+        view.startAnimation(animation)
+        view.visibility = View.INVISIBLE
+
     }
 }
