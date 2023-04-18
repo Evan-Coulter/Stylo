e891a2fd79eee9ecf3a54f9b77049b28f8ee725d
Evan-Coulter <evanrileycoulter@gmail.com>
2023-04-18 11:54:19 -0700

    make formatting buttons functional

--- app/src/main/java/com/example/stylo/editor/NoteEditorFragment.kt
+++ app/src/main/java/com/example/stylo/editor/NoteEditorFragment.kt
@@ -5,10 +5,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.EditText
-import android.widget.ImageButton
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
 import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
@@ -28,7 +25,10 @@ class NoteEditorFragment(private val note: RoomNote, private val folder: RoomFol
     private lateinit var italic: ImageButton
     private lateinit var bold: ImageButton
     private lateinit var underline: ImageButton
+    private lateinit var strikeThrough: ImageButton
+    private lateinit var bullets: ImageButton
     private lateinit var backButton: ImageButton
+    private lateinit var formatButtonLayout: LinearLayout
     private lateinit var noteTitle: EditText
     private lateinit var lastEditedTime: TextView
 
@@ -51,7 +51,7 @@ class NoteEditorFragment(private val note: RoomNote, private val folder: RoomFol
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initButtons(view)
-        initEditor(view)
+        initEditor()
     }
 
     private fun onNewState(newState: NoteEditorViewState) {
@@ -69,26 +69,33 @@ class NoteEditorFragment(private val note: RoomNote, private val folder: RoomFol
         italic = layout.findViewById<ImageButton>(R.id.italic).also { it.setOnClickListener { editor.setItalic() }}
         bold = layout.findViewById<ImageButton>(R.id.bold).also{ it.setOnClickListener { editor.setBold() }}
         underline = layout.findViewById<ImageButton>(R.id.underline).also{ it.setOnClickListener { editor.setUnderline() }}
+        strikeThrough = layout.findViewById<ImageButton>(R.id.strikethrough).also { it.setOnClickListener { editor.setStrikeThrough() } }
+        bullets = layout.findViewById<ImageButton>(R.id.bullets).also{ it.setOnClickListener { editor.setBullets() } }
+        formatButtonLayout = layout.findViewById<LinearLayout>(R.id.format_buttons).also { it.visibility = View.GONE; it.isEnabled = false }
         backButton = layout.findViewById<ImageButton>(R.id.back_button).also{ it.setOnClickListener {
             activity?.onBackPressed()
         }}
     }
 
 
-    private fun initEditor(layout: View) {
+    private fun initEditor() {
         editor.setOnTextChangeListener {
             viewModel._eventListener.value = NoteEditorEvent.NoteContentEdited(it)
         }
-        editor.focusEditor()
+        editor.setOnDecorationChangeListener { text, types ->
+            formatButtonLayout.let {
+                it.visibility = View.VISIBLE
+                it.isEnabled = true
+            }
+        }
         noteTitle.addTextChangedListener {
             viewModel._eventListener.value = NoteEditorEvent.NoteTitleEdited(it.toString())
         }
     }
 
     private fun showFullEditorState(note: RoomNote, folder: RoomFolder) {
-        view?.let { initButtons(it) }
-        listOf(bold, underline, italic, backButton).forEach {
-            it.setColorFilter(Color.parseColor(ColorStringMap.getColor(folder.color)), android.graphics.PorterDuff.Mode.SRC_IN)
+        listOf(bold, underline, italic, backButton, strikeThrough, bullets).forEach {
+            it.setColorFilter(Color.parseColor(ColorStringMap.getLightColor(folder.color)), android.graphics.PorterDuff.Mode.SRC_IN)
         }
         editor.html = note.content
         lastEditedTime.text = note.dateLastSaved.toString()
